bb93450799f84993b9104fb5eae10ca11486a2e6
Manel Huguet <joanmanuelhuguet@iesebre.com>
2015-10-06 19:46:54 +0200

    Afegeixo..

---
+++ Hello.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 06/10/15
+ * Time: 19:31
+ */
+class Hello
+{
+    public function sayHello()
+    {
+        echo "Hola mon!\n";
+    }
+}
---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . "/Hello.php";
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 06/10/15
+ * Time: 19:32
+ */
+$greetings = new Hello();
+$greetings ->sayHello();
+
